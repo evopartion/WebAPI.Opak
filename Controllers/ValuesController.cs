03e431ee5786a7f5cc4c92326a598bfdda1bc1d9
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-21 02:20:00 +0300

    OpenApi Öncesi

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Opak.Controllers
+{
+
+    [Route("api/Values/[action]")]
+    [ApiController]
+    public sealed class ValuesController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok(new {Message="Göksel"});
+        }
+
+        [HttpGet]
+        public IActionResult Hello2()
+        {
+            return StatusCode(200, new { Message = "Göksel" });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Birinci Parça
+// Service Registration
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// Ýkinci Parça
+// Middleware
+
+app.MapControllers();
 
 app.Run();
